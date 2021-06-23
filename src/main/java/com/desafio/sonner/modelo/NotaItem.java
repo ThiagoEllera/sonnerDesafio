fd4bb62012c17a3b1179c392c056e6f0a637d731
eliseu <eliseu@sonner.com.br>
2021-06-23 14:50:22 -0300

    Relacionamentos

--- src/main/java/com/desafio/sonner/modelo/NotaItem.java
+++ src/main/java/com/desafio/sonner/modelo/NotaItem.java
@@ -14,14 +14,18 @@ public class NotaItem {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	private Integer numero;
-	private BigDecimal quantidade;
+
 	@ManyToOne
 	private Nota nota;
 
+	private Integer numero;
+
 	@ManyToOne
 	private Produto produto;
-	
+
+	private BigDecimal quantidade;
+
+
 	public NotaItem() {
 		
 	}
